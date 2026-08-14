21002f7c5eb11666e62a87425da33ead6e1ea898
Isaacwilly14 <isaactumusiimewilly@gmail.com>
2026-08-14 06:35:03 +0300

    Adding more complex operands

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -35,14 +35,43 @@ println("++X = ${++X}")
 println("X-- = ${X--}")
 println("X-- = ${--X}")
 
-val myNumber = 4.5
-if (myNumber >= 5) {
-    println("The number is greater or equal to 5.")
-} else if (myNumber >= 4) {
-println("The number is greater than 4")
-}else {
-    println("The condition is false.")
+//val isActive = true
+//val score = 100
+//val internetSpeed = 4000
+//
+//if (isActive && score == 100 && internetSpeed == 5000) {
+//    println("You're at the next level.")
+//} else {
+//println("You're at the same level.")
+//
+//}
 
+    val num1 = 5
+    val num2 = 4
+
+//    val text = if (num1 >0 || num2 >0){
+//        println("The condition is true")
+//        "This is text 1"
+//    }else{
+//        println("The condition is false")
+//        "This is text 2"
+//    }
+//    println("$text")
+
+ //This gives you the same results as above but in a simplified version
+
+//    val text = if (num1 >0 || num2 >0){
+//        "This is text 1"
+//    }else{
+//        "This is text 2"
+//    }
+//    println("$text")
+
+if (num1 > 0 || num2 > 0) {
+    println("This condition is true")
+    println("This condition is true and some text")
+} else {
+    println("This condition is false")
+    println("This condition is false and some text")
 }
-
 }
